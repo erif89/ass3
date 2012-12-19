3fe499189b482d4dac4d5075dcd3b3e6b86d3f09
Emil <erif89@gmail.com>
2012-12-19 17:43:16 +0100

    Change Nod and Dict datatypes, update dictionary function types

--- assignment3.hs
+++ assignment3.hs
@@ -23,47 +23,45 @@ data Nil = Nothing
 
 -- An abstract data type for dictionaries, i.e., key-value stores,
 -- represented as an ordered tree.
-data Nod k v = Nod k v (Nod k v) (Nod k v) | Nil
+data Tree k v = Nil | Nod k v (Tree k v) (Tree k v)
+
+
 
 --instance Show Color where
 --    show Red   = "Red"
 --    show Green = "Green"
 --    show Blue  = "Blue"
     
---instance Show (Nod k v) where
---    show Nil = "Nil"
---    show (Nod k v l r) = "k " ++ show k ++ ", v " ++ show v ++
---                          ", l (" ++ show l ++
---                          "), r (" ++ show r ++ ")"
+instance (Show k, Show v) => Show (Tree k v) where
+    show Nil = "Nil"
+    show (Nod k v l r) = "k " ++ show k ++ ", v " ++ show v ++
+                          ", l (" ++ show l ++
+                          "), r (" ++ show r ++ ")"
 
 --instance Show (Nod k v) where
 --  show (Nod k v l r) = showNod (Nod k v l r)
 --  show Nil = showNod Nil
 
-data Dict k v cmp = Root v (Nod k v) cmp
-
---instance Show (Dict k v cmp) where
---  show (Root d n cmp) = "{Dict: " ++ show n ++ "}"
+data Dict k v = Root v (Tree k v) (k -> k -> Ordering)
 
     
 
 -- create a new empty dictionary with compare being the comparison function to be used for keys. The comparison function should take two keys and return one of the constants LT, EQ, GT to express the relationship between the keys. d is the default value that should be returned if a key is not found.
--- createDictionary :: cmp v -> Dict k v cmp -- TODO change cmp to function type
-createDictionary :: c -> b -> Dict x b c -- FIXME
+createDictionary :: (k -> k -> Ordering) -> v -> Dict k v
 createDictionary compare d = Root d Nil compare
 
 -- find value for key.
-find :: k -> Dict k v c -> v
+find :: k -> Dict k v -> v
 find key (Root d Nil _) = d
 find key (Root d (Nod k v left right) cmp) = v -- FIXME
 
 -- return a new dictionary where key now maps to value, regardless of if it was present before.
-update :: k -> v -> Dict k v (k -> k -> Ordering) -> Dict k v (k -> k -> Ordering)
+update :: k -> v -> Dict k v -> Dict k v
 update key value (Root d Nil cmp) = (Root d (Nod key value Nil Nil) cmp)
 update key value (Root d node cmp) = (Root d (update_node key value node cmp) cmp)
     
 --update_node :: k -> v -> Nod k v -> c -> Nod k v
-update_node :: k -> v -> Nod k v -> (k -> k -> Ordering) -> Nod k v
+update_node :: k -> v -> Tree k v -> (k -> k -> Ordering) -> Tree k v
 update_node key value (Nod k v Nil Nil) cmp = (Nod k v Nil Nil)
 update_node key value (Nod k v Nil right) cmp
     | cmp key k == EQ = (Nod k value Nil right)
@@ -81,21 +79,21 @@ update_node key value (Nod k v left right) cmp
     | cmp key k == GT = (Nod k v left (update_node key value right cmp))
 
 -- fold the key-value pairs of the dictionary using the function fun. fun should take three arguments: key, value and sofar (in this order) which is the accumulated value so far. initial is the initial value for sofar. Please note that order of application is (or at least should be) not relevant.
-fold :: (k -> v -> s) -> Dict k v c -> s -> s 
+fold :: (k -> v -> s) -> Dict k v -> s -> s 
 fold fun dict initial = initial -- FIXME
 
 -- return a new dictionary that is more balanced (if needed). This could be run when needed as part of update as well.
 -- rebalace :: (Dict k v cmp) -> (Dict k v cmp) FIXME
-rebalance :: Dict k v c -> Dict k v c  -- FIXME
+rebalance :: Dict k v -> Dict k v  -- FIXME
 rebalance dict = dict -- FIXME
 
 -- return the keys of the dictionary in a list. The order of the keys is not relevant.
-keys :: (Dict k v cmp) -> [k]
+keys :: (Dict k v) -> [k]
 keys dict = [] -- FIXME
 
 -- determines if dict1 and dict2 contain the same set of keys. Take care to make it efficient, i.e., do not construct unnecessary large intermediate data structures. samekeys should use the compare function, which should be the same for the two dictionaries and can be assumed to behave 'in the right way' for its two arguments (i.e. EQ return value implies that the arguments can be swapped and the result is still EQ, GT implies that if the arguments are swapped the return value will be LT and vice-versa).
 -- samekeys :: (Dict k v cmp) (Dict k v cmp) -> Bool -- FIXME
-samekeys :: Dict k v c -> Dict k v c -> Bool -- FIXME
+samekeys :: Dict k v -> Dict k v -> Bool -- FIXME
 samekeys dict1 dict2 = False -- FIXME
 
 
